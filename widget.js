94c21e5d411147440baec31069d05715c1e4ceb4
khurram44 <ks7844201@gmail.com>
2024-08-18 02:22:48 +0500

    googleadd

--- widget.js
+++ widget.js
@@ -4,9 +4,11 @@ import { Grid, Card, CardContent, Typography, Avatar, CardHeader, Button, Rating
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import './widget.css'
+
 const TruncatedText = ({ text, maxLength }) => {
     const [isExpanded, setIsExpanded] = useState(false);
     const handleToggle = () => setIsExpanded(!isExpanded);
+
     return (
         <Typography variant="body2" color="text.secondary">
             {isExpanded ? text : `${text.substring(0, maxLength)}${text.length > maxLength ? '...' : ''}`}
@@ -18,6 +20,7 @@ const TruncatedText = ({ text, maxLength }) => {
         </Typography>
     );
 };
+
 const Widget = () => {
     const [cardData, setCardData] = useState([]);
 
@@ -26,7 +29,7 @@ const Widget = () => {
             try {
                 const response = await fetch('https://staged-loanshub-96be18a8af38.herokuapp.com/reviews?keywords=application%20portal,loanshub');
                 const data = await response.json();
-                setCardData(data); // Assuming the API response has a `reviews` array
+                setCardData(data);
             } catch (error) {
                 console.error('Error fetching data:', error);
             }
@@ -39,23 +42,22 @@ const Widget = () => {
         infinite: true,
         speed: 500,
         slidesToShow: 3,
-        slidesToScroll: 1,
+        slidesToScroll: 3,
         responsive: [
             {
-                breakpoint: 960, // tablet breakpoint
+                breakpoint: 960,
                 settings: {
                     slidesToShow: 2,
                 },
             },
             {
-                breakpoint: 600, // mobile breakpoint
+                breakpoint: 600,
                 settings: {
                     slidesToShow: 1,
                 },
             },
         ],
     };
-
     return (
         <Grid container spacing={3} justifyContent="center" style={{ padding: '20px' }}>
             <Grid item xs={12}>
@@ -105,6 +107,21 @@ const Widget = () => {
                             </Card>
                         </div>
                     ))}
+                    {/* Last card with image and button */}
+                    <div style={{ marginRight: '15%' }}>
+                        <Card style={{ width: '90%', margin: '0 auto', textAlign: 'center', padding: '20px', minHeight: '192px' }}>
+                            <img
+                                src="https://www.greenworldholidays.com/wp-content/uploads/2019/12/google-verified-reviews.jpg"
+                                alt="Placeholder"
+                                style={{ width: '220px', height: '110px', margin: 'auto' }}
+                            />
+                            <Button variant="contained" color="primary" style={{ marginTop: '3%' }}
+                                onClick={() => window.open('https://www.google.com/maps/place/loansHub+-+Your+local+home+loan+experts+online/@-24.7593997,114.3099059,4z/data=!4m8!3m7!1s0x6b91592c752fc429:0x8494743c28332fa5!8m2!3d-26.4420923!4d136.013279!9m1!1b1!16s%2Fg%2F11gzqbnppv?entry=ttu', '_blank')}
+                            >
+                                View All Reviews
+                            </Button>
+                        </Card>
+                    </div>
                 </Slider>
             </Grid>
         </Grid>
